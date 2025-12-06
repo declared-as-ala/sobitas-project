ac612d7f5be04d29d68b885a5450ff26bd6e6179
wajih <wajih@softhub.softhub.tn>
2025-12-06 16:22:13 +0100

    desktop slide number 2 changed

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -33,7 +33,7 @@ export class HomeComponent implements OnInit {
         "type": "web"
     },
     {
-        "cover": "assets/img/slides/sobitas_slide_2.webp",
+        "cover": "assets/img/slides/sobitas_slide_22.webp",
         "designation_fr": "Sobitas, votre boutique de compléments alimentaires en ligne en Tunisie",
         "description_fr": null,
         "btn_text_fr": null,
