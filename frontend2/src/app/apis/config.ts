5a20f477031f1b50a2d3574d71079e780b89694f
wajih <wajih@softhub.softhub.tn>
2026-01-18 10:21:22 +0100

    new db file added

--- frontend2/src/app/apis/config.ts
+++ frontend2/src/app/apis/config.ts
@@ -1,6 +1,6 @@
 //export const host = 'https://admin.protein.tn/api/'
-export const host = 'https://admin.sobitas.tn/api/'
-export const storage = 'https://admin.sobitas.tn/storage/'
+export const host = 'https://admin.protein.tn/api/'
+export const storage = 'https://admin.protein.tn/storage/'
 export const environment = {
   production: true,
   baseUrl: 'https://protein.tn'
