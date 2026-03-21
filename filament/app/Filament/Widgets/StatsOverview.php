66a1719a7d831e0169993b9f493a8aabeaf321f6
ahmedbenhamouda1 <you-can-put-ahmed-email@example.com>
2026-03-21 19:33:31 +0100

    hh

--- filament/app/Filament/Widgets/StatsOverview.php
+++ filament/app/Filament/Widgets/StatsOverview.php
@@ -13,7 +13,7 @@ use Livewire\Attributes\On;
 
 class StatsOverview extends BaseWidget
 {
-    protected static string $view = 'filament.widgets.stats-overview';
+    protected string $view = 'filament.widgets.stats-overview';
 
     #[On('dashboardFilterUpdated')]
     public function refresh(): void
